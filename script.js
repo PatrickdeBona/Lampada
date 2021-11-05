a2dfddbc19e5ba230a89df9a824cefcd81b2ce18
PatrickdeBona <patrickdebonademarco@live.com>
2021-11-05 19:31:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const lampada = document.getElementById('lampada');
 const interrupitor = document.getElementById('interrupitor')
 
+
 function lampOn () {
     lampada.src = 'imgs/ligada.jpg';
 }
 
 function lampOff () {
-    if(lampOn ()){
     lampada.src = 'imgs/desligada.jpg';
-    }
 }
 
-    interrupitor.addEventListener ('click', lampOff);
-    interrupitor.addEventListener ('click', lampOn);
+function lampBroken () {
+    lampada.src = 'imgs/quebrada.jpg'
+}
+    lampada.addEventListener ('mouseover', lampOn);
+    lampada.addEventListener ('mouseleave', lampOff);
+    lampada.addEventListener ('dbclick', lampBroken);
