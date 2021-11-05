d64d2523fbce2fcd856204aee9c49feaad85a62b
PatrickdeBona <patrickdebonademarco@live.com>
2021-11-05 19:38:57 -0300

    Lampada

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 const lampada = document.getElementById('lampada');
 const interrupitor = document.getElementById('interrupitor')
 
+function isLampBroken () {
+    return lampada.src.indexOf ('quebrada') > -1;
+}
+
 
 function lampOn () {
+    if( !isLampBroken ())
     lampada.src = 'imgs/ligada.jpg';
 }
 
 function lampOff () {
+    if( !isLampBroken ())
     lampada.src = 'imgs/desligada.jpg';
 }
 
 function lampBroken () {
-    lampada.src = 'imgs/quebrada.jpg'
+    lampada.src = 'imgs/quebrada.jpg';
 }
     lampada.addEventListener ('mouseover', lampOn);
     lampada.addEventListener ('mouseleave', lampOff);
-    lampada.addEventListener ('dbclick', lampBroken);
+    lampada.addEventListener ('dblclick', lampBroken);
